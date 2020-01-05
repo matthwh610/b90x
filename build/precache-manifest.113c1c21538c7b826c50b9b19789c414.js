ed67ae702e422a39a017637fe0f6bd85e4b2db98
Matthew Haggerty <matthewhaggerty@gmail.com>
2020-01-05 08:19:19 -0500

    build files for v1.0

--- build/precache-manifest.113c1c21538c7b826c50b9b19789c414.js
+++ build/precache-manifest.113c1c21538c7b826c50b9b19789c414.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "7405f6d47e4ac0f7c690",
-    "url": "/static/js/main.bb02a462.chunk.js"
+    "revision": "a9891e50a24b4908453e",
+    "url": "/static/js/main.910fdd16.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/media/CharlotteBible.bbe0c7ec.jpg"
   },
   {
-    "revision": "d4b70d3d202854a8bb66c88f3e7818b9",
+    "revision": "ce8740902f4072edc80300ae9f727c50",
     "url": "/index.html"
   }
 ];
